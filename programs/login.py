dad1bb514133b5c4fd87bbbdab606309ef6734e7
Meaxis <37759268+Meaxis@users.noreply.github.com>
2021-06-30 14:17:38 +0200

    suffering
    God I hate Tkinter

--- programs/login.py
+++ programs/login.py
@@ -11,8 +11,8 @@ app = Tk()
 width = app.winfo_screenwidth()
 height = app.winfo_screenheight()
 
-window_height = str(math.floor(height/2))
-window_width = str(math.floor(width/5))
+window_height = str(math.floor(height / 2))
+window_width = str(math.floor(width / 5))
 
 app.title("Login to your MeaxisNetwork Account")
 app.resizable(False, False)
@@ -20,21 +20,26 @@ app.configure(background="#1c1c20")
 app.overrideredirect(True)
 
 if platform.system() == "Windows":
-	monitor_info = GetMonitorInfo(MonitorFromPoint((0, 0)))
-	monitor_area = monitor_info.get("Monitor")
-	work_area = monitor_info.get("Work")
-	taskbar_height = format(monitor_area[3]-work_area[3])
+    monitor_info = GetMonitorInfo(MonitorFromPoint((0, 0)))
+    monitor_area = monitor_info.get("Monitor")
+    work_area = monitor_info.get("Work")
 
-	width = width - int(window_width)
-	height = height - int(window_height)
+    width = work_area[2]
+    height = work_area[3]
 
-	geometry_string = window_width + "x" + window_height + "+" + str(width) + "+" + str(
-		height)
+    width = math.floor((width - int(window_width)) / 2)
+    height = math.floor((height - int(window_height)) / 2)
 
-	print(geometry_string)
+    geometry_string = window_width + "x" + window_height + "+" + str(width) + "+" + str(
+        height)
 else:
-	geometry_string = window_width + "x" + window_height + "+" + str(width - int(window_width)) + "+" + str(
-		height - int(window_height))
+    geometry_string = window_width + "x" + window_height + "+" + str(width - int(window_width)) + "+" + str(
+        height - int(window_height))
+
+# Window contents
+welcome_title = Label(app, text="Test")
+welcome_title.grid(row = 0, column = 0, sticky = W, pady = 2)
+welcome_title.grid(row = 1, column = 0, sticky = W, pady = 2)
 
 app.geometry(geometry_string)
 app.mainloop()
